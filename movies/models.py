373ee8fda866383270f9738e911e1a8db9e4ad20
Lucía Prado Fernández-Vega <202112679@alu.comillas.edu>
2024-05-12 14:38:21 +0200

    [UPDATE] Movie model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    year = models.CharField(max_length=4)
+    director = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    duration = models.CharField(max_length=10)
+    rating = models.FloatField()
+    votes = models.IntegerField()
+    image = models.CharField(max_length=100)
+    plot = models.TextField()
+    trailer = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
