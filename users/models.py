e5690fd48378b2533d6c86ba8acfa09a9e937290
DESKTOP-ABB8PBE\pablo <202110501@alu.comillas.edu>
2024-05-14 01:08:21 +0200

    [UPDATE] Added simple user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+
+    name = models.CharField(max_length=256)
+    email = models.EmailField()
+    password = models.CharField(max_length=128)
+
+    def save(self, *args, **kwargs):
+        if not self.username:
+            self.username = self.email
+        
+        super().save(*args, **kwargs)
