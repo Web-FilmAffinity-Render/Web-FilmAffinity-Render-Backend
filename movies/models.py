62326dee67211266ef74ac12acfdfd8c56466bb4
DESKTOP-ABB8PBE\pablo <202110501@alu.comillas.edu>
2024-05-14 09:29:33 +0200

    [UPDATE] Change Movie model for a simpler initial approach. Removed category and image

--- movies/models.py
+++ movies/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     year = models.IntegerField()
@@ -8,10 +7,9 @@ class Movie(models.Model):
     director = models.CharField(max_length=100)
     cast = models.CharField(max_length=600)
     rate = models.CharField(max_length=10)
-    category = models.CharField(max_length=100)
     genre = models.CharField(max_length=100)
     duration = models.BigIntegerField()
-    image = models.CharField(max_length=100)
+    # image = models.CharField(max_length=100)
     plot = models.TextField()
 
     def __str__(self):
