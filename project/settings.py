598786ad3f7796e4ef8d9487c240ff7919512a15
DESKTOP-ABB8PBE\pablo <202110501@alu.comillas.edu>
2024-05-14 08:34:20 +0200

    [UPDATE] Updated and migrated migrations for movies, users and reviews

--- project/settings.py
+++ project/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "movies.apps.MoviesConfig",
+    "users.apps.UsersConfig",
+    "reviews.apps.ReviewsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
