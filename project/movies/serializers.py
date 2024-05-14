f6c1c6ff13101024725f8c8e08c03986e13adc77
andresmartinezfuentes <112613478+andresmartinezfuentes@users.noreply.github.com>
2024-05-14 22:44:09 +0200

    Update serializers.py

--- project/movies/serializers.py
+++ project/movies/serializers.py
@@ -7,7 +7,20 @@ class MovieSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
     def create(self, validated_data):
-      return models.Movie.objects.create_movie(username=validated_data['title'], **validated_data)
+      return models.Movie.objects.create_movie(title=validated_data['title'], **validated_data)
 
     def update(self, instance, validated_data):
        return super().update(instance, validated_data)
+
+class MovieListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Movie
+        fields = '__all__'
+        
+    def validate_stock(self, value):
+        if value == 0:
+            raise serializers.ValidationError("Stock cannot be zero")
+        return value
+    
+    def create(self, validated_data):
+        return models.Movie.objects.create(**validated_data)
